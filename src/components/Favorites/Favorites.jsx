6f3b698d89acc3f77a277ac473c45ce6112da6ba
Franco Rodriguez Dodera <francododera@hotmail.com>
2023-05-10 18:01:42 -0300

    Order y filter

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,39 +1,61 @@
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import Card from "../Card/Card";
 import style from "./Favorites.module.css";
+import { filterCards, orderCards } from "../../redux/action";
 
 const Favorites = ({ myFavorites }) => {
+  const dispatch = useDispatch();
+  const handleOrder = (event) => {
+    dispatch(orderCards(event.target.value));
+  };
+  const handleFilter = (event) => {
+    dispatch(filterCards(event.target.value));
+  };
+
   return (
-    <div className={style.container}>
-      {myFavorites.map(
-        ({
-          id,
-          name,
-          status,
-          gender,
-          origin,
-          species,
-          image,
-          onClose,
-          addFav,
-          removeFav,
-          myFavorites,
-        }) => {
-          return (
-            <Card
-              id={id}
-              name={name}
-              status={status}
-              gender={gender}
-              origin={origin}
-              species={species}
-              image={image}
-              onClose={onClose}
-              removeFav={removeFav}
-            />
-          );
-        }
-      )}
+    <div>
+      <select onChange={handleOrder}>
+        <option value="A">Ascendente</option>
+        <option value="D">Descendente</option>
+      </select>
+
+      <select onChange={handleFilter}>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+        <option value="Genderless">Genderless</option>
+        <option value=" unknown"> unknown</option>
+      </select>
+      <div className={style.container}>
+        {myFavorites.map(
+          ({
+            id,
+            name,
+            status,
+            gender,
+            origin,
+            species,
+            image,
+            onClose,
+            addFav,
+            removeFav,
+            myFavorites,
+          }) => {
+            return (
+              <Card
+                id={id}
+                name={name}
+                status={status}
+                gender={gender}
+                origin={origin}
+                species={species}
+                image={image}
+                onClose={onClose}
+                removeFav={removeFav}
+              />
+            );
+          }
+        )}
+      </div>
     </div>
   );
 };
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,18 @@
-import { ADD_FAV, REMOVE_FAV } from "./action";
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./action";
 
 const initialState = {
   myFavorites: [],
+  allCharacters: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FAV:
-      return { ...state, myFavorites: [...state.myFavorites, action.payload] };
+      return {
+        ...state,
+        myFavorites: [...state.allCharacters, action.payload],
+        allCharacters: [...state.allCharacters, action.payload],
+      };
     case REMOVE_FAV:
       return {
         ...state,
@@ -15,7 +20,29 @@ const reducer = (state = initialState, action) => {
           (char) => char.id !== action.payload
         ),
       };
-
+    case FILTER:
+      const filteredCharacters = state.allCharacters.filter(
+        (char) => char.gender === action.payload
+      );
+      return {
+        ...state,
+        myFavorites: filteredCharacters,
+      };
+    case ORDER:
+      const sortedCharacters = [...state.allCharacters];
+      sortedCharacters.sort((a, b) => {
+        if (action.payload === "A") {
+          return a.id - b.id; // Orden ascendente
+        } else if (action.payload === "D") {
+          return b.id - a.id; // Orden descendente
+        } else {
+          return 0;
+        }
+      });
+      return {
+        ...state,
+        myFavorites: sortedCharacters,
+      };
     default:
       return { ...state };
   }
